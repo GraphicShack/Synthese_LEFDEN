736419ace57fc1d083c4cb2585335108bb78aed1
ElliDen <143757316+ElliDen@users.noreply.github.com>
2023-11-20 11:19:45 -0500

    pause update

--- Assets/_MesAssets/Scripts/GestionUiJeux.cs
+++ Assets/_MesAssets/Scripts/GestionUiJeux.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GestionUiJeux : MonoBehaviour
 {
@@ -7,10 +8,13 @@ public class GestionUiJeux : MonoBehaviour
     public TextMeshProUGUI tirText;
     public TextMeshProUGUI ennemiAbattuText;
     public TextMeshProUGUI scoreFinalText;
-
+    //public TextMeshProUGUI _txtQuit;
+    public TextMeshProUGUI _txtRestart;
+    public GameObject _pausePanel;
     private int tempsDeJeuEnSecondes = 0;
     private int tirs = 0;
     private int ennemisAbattus = 0;
+    private bool _pauseOn = false;
 
     void Start()
     {
@@ -23,6 +27,27 @@ public class GestionUiJeux : MonoBehaviour
         tempsText.text = "Temps : " + tempsDeJeuEnSecondes;
 
         MettreAJourScoreFinal();
+        if (_txtRestart.gameObject.activeSelf && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        else if (_txtRestart.gameObject.activeSelf && Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        if ((Input.GetKeyDown(KeyCode.Escape) && !_txtRestart.gameObject.activeSelf) && !_pauseOn)
+        {
+            _pausePanel.SetActive(true);
+            Time.timeScale = 0;
+            _pauseOn = true;
+        }
+        else if ((Input.GetKeyDown(KeyCode.Escape) && !_txtRestart.gameObject.activeSelf) && _pauseOn)
+        {
+            _pausePanel.SetActive(false);
+            Time.timeScale = 1;
+            _pauseOn = false;
+        }
     }
 
     public void AugmenterTir()
@@ -49,5 +74,11 @@ public class GestionUiJeux : MonoBehaviour
         tirText.text = "Tir : " + tirs;
         ennemiAbattuText.text = "Ennemi abattu : " + ennemisAbattus;
         MettreAJourScoreFinal();
+    }
+    public void ResumeGame()
+    {
+        _pausePanel.SetActive(false);
+        Time.timeScale = 1;
+        _pauseOn = false;
     }
 }
